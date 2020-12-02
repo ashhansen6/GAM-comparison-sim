f24f9a1cd965758e2b5a72182f2abffa004ed7ae
Ashton Hansen <cdf&1456>
2020-12-02 15:51:24 -0600

    Changing randomizations for predictors, adding training and test sets

--- data_fab.R
+++ data_fab.R
@@ -2,35 +2,16 @@
 set.seed(1456)
 
 # Predictor Variables
-x1 <- rnorm(1000, mean = rnorm(1, mean=54, sd=12), sd = rnorm(1, mean=4, sd=2))
-hist(x1)
-
-x2 <- rnorm(1000, 131, 16)
-hist(x2)
-
-x3 <- rgamma(1000, shape = rnorm(1, 5, 2), rate = rnorm(1, 15, 5))
-hist(x3)
-
-x4 <- rexp(1000, rate = 10)
-hist(x4)
-
-x5 <- runif(1000, min = 30, max = 50)
-hist(x5)
-
-x6 <- rchisq(1000, 15)
-hist(x6)
-
-x7 <- rpois(1000, 15)
-hist(x7)
-
-x8 <- rexp(1000, 2)
-hist(x8)
-
-x9 <- rlogis(1000, location = 60, scale = 2)
-hist(x9)
-
-x10 <- rgamma(1000, shape = 2, rate = 4)
-hist(x10)
+x1 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x2 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x3 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x4 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x5 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x6 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x7 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x8 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x9 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x10 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
 
 # Predicted Variables
 
@@ -46,5 +27,15 @@ hist(y3)
 y4 <- (log(x1 + x9) - sin(x8^3))/x2 + x1
 hist(y4)
 
+# Data Frame
 df <- data.frame(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, y1, y2, y3, y4)
+df <- df %>% mutate(id = row_number())
 
+round(as.matrix(cor(df[1:10])), 4)
+
+# Test and Training Sets
+train <- sample_n(df, 800)
+test <- anti_join(df, train, by = 'id')
+
+glimpse(test)
+glimpse(train)
