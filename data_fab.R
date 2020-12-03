193cc8c2976f46ac7e0451d15a4c36ec3f4cb353
Ashton Hansen <cdf&1456>
2020-12-03 00:19:15 -0600

    Shaking up data, adding LASSO models file, adding plots for planning GAMs

--- data_fab.R
+++ data_fab.R
@@ -1,14 +1,17 @@
+# Packages used:
+library(tidyverse)
+
 # Seed
 set.seed(1456)
 
 # Predictor Variables
 x1 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
-x2 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x2 <- runif(1000, rnorm(1, 30, 4), rnorm(1, 55, 8))
 x3 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
-x4 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x4 <- runif(1000, 30, 60)
 x5 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
 x6 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
-x7 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
+x7 <- runif(1000, rnorm(1, 40, 10), rnorm(1, 77, 3))
 x8 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
 x9 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
 x10 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
@@ -18,17 +21,14 @@ x10 <- rnorm(1000, mean = rnorm(1, 50, 10), sd = rnorm(1, 10, 3))
 y1 <- x1 + x3 + x6 + x10 - x5 - x8
 hist(y1)
 
-y2 <- x2^2 + x5^2 + x6 + x7 - x9^2
+y2 <- x1^2 + x5^2 + x6 - x9^2
 hist(y2)
 
-y3 <- x1^3 + x8^4 + x10^2 - x2^3 - x7
+y3 <- ((3*(3*x1 + (x9 - 38))) - (x8 + x6)/x2 + x3/x7)*2*x10
 hist(y3)
 
-y4 <- (log(x1 + x9) - sin(x8^3))/x2 + x1
-hist(y4)
-
 # Data Frame
-df <- data.frame(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, y1, y2, y3, y4)
+df <- data.frame(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, y1, y2, y3)
 df <- df %>% mutate(id = row_number())
 
 round(as.matrix(cor(df[1:10])), 4)
